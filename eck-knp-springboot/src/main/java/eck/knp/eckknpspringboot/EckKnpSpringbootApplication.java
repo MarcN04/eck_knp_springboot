2fc7e903755e08b29a3b4d6e6d2fc6a668251899
Marc <Marc.Niedermeier@t-systems.com>
2022-02-16 20:19:26 +0100

    Initial commit

---
+++ eck-knp-springboot/src/main/java/eck/knp/eckknpspringboot/EckKnpSpringbootApplication.java
@@ -0,0 +1,13 @@
+package eck.knp.eckknpspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EckKnpSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EckKnpSpringbootApplication.class, args);
+	}
+
+}
---
+++ eck-knp-springboot/src/test/java/eck/knp/eckknpspringboot/EckKnpSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package eck.knp.eckknpspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EckKnpSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
